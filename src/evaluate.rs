7b19fd9990761967e814363bea21a8923a4f244c
tretikov <max@tretikov.com>
2023-07-03 23:42:12 -0700

    Advantages outlines

--- src/evaluate.rs
+++ src/evaluate.rs
@@ -14,7 +14,6 @@ pub type StockAdvantage = f32;
 
 pub fn checkmate(port: &PortData, stage: peppi::model::enums::stage::Stage) -> bool {
 	// If any velocity plus position is past a blast zone, return true
-	// println!("Port: {:?}", port);
 
     let data = port.leader.post;
 
@@ -27,27 +26,40 @@ pub fn checkmate(port: &PortData, stage: peppi::model::enums::stage::Stage) -> b
         y: data.position.y + current_velocity.knockback.y + current_velocity.autogenous.y,
     };
 
-    println!("Character: {:?}", data.character);
-    println!("Current position: {:?}", data.position);
-    println!("Velocities: {:?}", current_velocity);
-    println!("Next position: {:?}", next_position);
-    println!("Blasts: {:?}", stage_data.blasts);
-
     if 
         next_position.x < stage_data.blasts.left || 
         next_position.x > stage_data.blasts.right || 
         next_position.y < stage_data.blasts.bottom || 
         next_position.y > stage_data.blasts.top 
     {
-        println!("Checkmate!");
         return true;
     }
 
 	return false;
 }
 
-pub fn position<const N: usize>(frame: &Frame<N>) -> StockAdvantage {
-	// println!("Frame: {:?}", frame);
+pub fn percentage_advantage(port: &PortData) -> StockAdvantage {
+	// Players at a higher percentage are at a disadvantage
+
+	return 0.;
+}
+
+pub fn positional_advantage(port: &PortData, stage: peppi::model::enums::stage::Stage) -> StockAdvantage {
+    // Player further from center of stage is at a disadvantage
+
+    // Player further below the stage is at a disadvantage
+
+	return 0.;
+}
+
+pub fn frame_advantage(port: &PortData) -> StockAdvantage {
+	// Players stuck in a commited action are at a disadvantage
+
+	return 0.;
+}
+
+pub fn intangibility_advantage(port: &PortData) -> StockAdvantage {
+	// Players close to an intangible player are at a disadvantage
 
 	return 0.;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -11,17 +11,15 @@ pub struct Evaluator<const N: usize> {
 
 impl Evaluator<1> {
     pub fn evaluate(&self) -> evaluate::StockAdvantage {
-        // Your implementation for Frame<1> here.
         return 0.;
     }
 }
 
 impl Evaluator<2> {
     pub fn evaluate(&self) -> evaluate::StockAdvantage {
-        // Your implementation for Frame<2> here.
-        if evaluate::checkmate(&self.frame.ports[0], self.stage) {
+        if evaluate::checkmate(&self.frame.ports[1], self.stage) {
             return 1.;
-        } else if evaluate::checkmate(&self.frame.ports[1], self.stage) {
+        } else if evaluate::checkmate(&self.frame.ports[0], self.stage) {
             return -1.;
         }
 
@@ -31,14 +29,12 @@ impl Evaluator<2> {
 
 impl Evaluator<3> {
     pub fn evaluate(&self) -> evaluate::StockAdvantage {
-        // Your implementation for Frame<3> here.
         return 0.;
     }
 }
 
 impl Evaluator<4> {
     pub fn evaluate(&self) -> evaluate::StockAdvantage {
-        // Your implementation for Frame<4> here.
         return 0.;
     }
 }
